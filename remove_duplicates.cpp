39f41802861a9d7e9a0fedfaee1fec8caf057639
cipher9 <davidscho9@gmail.com>
2020-07-21 16:32:14 -0700

    commit remove dup

---
+++ remove_duplicates.cpp
@@ -0,0 +1,28 @@
+/*
+    A simple program that takes in input string and push back into a vector.
+    The program outputs the number of words in the vector, then sorts the vector
+    to output the words without any duplicates. To terminate input stream Ctrl+Z
+    in Windows or Ctrl+D in Unix.
+*/
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<string> words;
+    for(string temp; cin>>temp;)
+        words.push_back(temp);
+
+    cout << "\nNumber of words: " << words.size() << '\n';
+    sort(words.begin(), words.begin()+words.size());
+
+    for(int i = 0; i<words.size(); ++i)
+        if(i==0||words[i-1]!=words[i])
+            cout<<words[i]<<'\n';
+    
+    return 0;
+}
