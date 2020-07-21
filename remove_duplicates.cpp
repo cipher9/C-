c948fb85e932260b610ee582fd6f70428ac7e61a
cipher9 <davidscho9@gmail.com>
2020-07-21 16:45:14 -0700

    added | to stop input stream

--- remove_duplicates.cpp
+++ remove_duplicates.cpp
@@ -2,7 +2,7 @@
     A simple program that takes in input string and push back into a vector.
     The program outputs the number of words in the vector, then sorts the vector
     to output the words without any duplicates. To terminate input stream Ctrl+Z
-    in Windows or Ctrl+D in Unix.
+    in Windows or Ctrl+D in Unix or input "|" and then press enter.
 */
 #include <iostream>
 #include <string>
@@ -15,7 +15,11 @@ int main()
 {
     vector<string> words;
     for(string temp; cin>>temp;)
+    {
         words.push_back(temp);
+        if(temp=="|")
+            break;
+    }
 
     cout << "\nNumber of words: " << words.size() << '\n';
     sort(words.begin(), words.begin()+words.size());
